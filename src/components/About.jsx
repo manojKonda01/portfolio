c977bf0e3a477ec48d6b4684b1e082160a674cb4
Manoj Konda <manojkonda@Manojs-MacBook-Pro.local>
2025-09-25 23:33:44 -0500

    about us section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,69 @@
 import ShinyCard from "./ShinyCard";
 import '../styles/About.css';
 
-const About = () =>{
+const About = () => {
     const currentYear = new Date().getFullYear();
+    const cardsStyle = {
+        width: '10rem',
+        height: '8rem',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
     return (
-        <section id="about" className="section about-section">
-            <div className="container d-flex justify-content-center align-items-center">
-                <ShinyCard style={{ }}>
-                    <div className="experience-card d-flex justify-content-center flex-column align-items-center">
-                        <div className="d-flex justify-content-center align-items-flex-end w-100">
-                            <div className="experience-number">{currentYear - 2021}<span>+</span></div>
-                            <div className="experience-year-label">years</div>
-                        </div>
-                        <div className="experience-label">experience</div>
+        <section id="aboutme" className="aboutme-section">
+            <div className="container d-flex justify-content-center align-items-center flex-column">
+                <div className="aboutme-title-container d-flex justify-content-center align-items-center">
+                    <div className="aboutme-title silver-text">From Pixels to Pipelines — I Build It All</div>
+                </div>
+                <div className="aboutme-content-container d-flex justify-content-center align-items-center">
+                    <div className="aboutme-right d-flex justify-content-center align-items-start">
+                        <ShinyCard style={{ marginBottom: '20px' }}>
+                            <div className="aboutme-right-title silver-text">
+                                Engineering Experiences End-to-End
+                            </div>
+                            <p className='aboutme-content'>I build at the intersection of data, cloud, and user experience. From sleek, responsive websites to scalable full-stack applications and data pipelines, I craft systems that work seamlessly and look intentional.</p>
+                            <p className='aboutme-content'>From shaping APIs and managing databases to optimizing cloud workflows, I craft solutions that are reliable, scalable, and impactful. Working with me means transforming concepts into systems that not only work, but thrive.</p>
+                        </ShinyCard>
                     </div>
-                </ShinyCard>
+                    <div className="aboutme-left">
+                        <div className="aboutme-cards">
+                            <ShinyCard style={cardsStyle}>
+                                <div className="experience-card d-flex justify-content-center flex-column align-items-center">
+                                    <div className="d-flex justify-content-center align-items-flex-end w-100">
+                                        <div className="experience-number">{currentYear - 2021}<span>+</span></div>
+                                        <div className="experience-year-label">years</div>
+                                    </div>
+                                    <div className="experience-label">experience</div>
+                                </div>
+                            </ShinyCard>
+                            <ShinyCard style={cardsStyle}>
+                                <div className="experience-card d-flex justify-content-center flex-column align-items-center">
+                                    <div className="d-flex justify-content-center align-items-flex-end w-100">
+                                        <div className="experience-number">10<span>+</span></div>
+                                    </div>
+                                    <div className="experience-label">websites</div>
+                                </div>
+                            </ShinyCard>
+                            <ShinyCard style={cardsStyle}>
+                                <div className="experience-card d-flex justify-content-center flex-column align-items-center">
+                                    <div className="d-flex justify-content-center align-items-flex-end w-100">
+                                        <div className="experience-number">3</div>
+                                    </div>
+                                    <div className="experience-label">webapps</div>
+                                </div>
+                            </ShinyCard>
+                            <ShinyCard style={cardsStyle}>
+                                <div className="experience-card d-flex justify-content-center flex-column align-items-center">
+                                    <div className="d-flex justify-content-center align-items-flex-end w-100">
+                                        <div className="experience-number">4</div>
+                                    </div>
+                                    <div className="experience-label">dataflows</div>
+                                </div>
+                            </ShinyCard>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
